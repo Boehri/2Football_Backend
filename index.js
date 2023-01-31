ac55a4b87bad7aa445ea175b9f1efdb9ff4f389f
Noah Böhringer <noahboehringer@Noahs-MacBook-Air.local>
2023-01-31 14:56:30 +0200

    added Basti

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+const app = express();
 
-const port = 4567
+const port = 4567;
 
 const dogs = [
-    {name: "Sam", breed: "Husky"}, 
-    {name: "Josh", breed: "Lab"}
-]
+  {name: "Sam", breed: "Husky"},
+  {name: "Josh", breed: "Lab"},
+  {name: "Basti", breed: "Retriever"},
+];
 
 app.get("/", (req, res) => {
-    res.json(dogs)
-})
+  res.json(dogs);
+});
 
 app.get("/dogs/:id", (req, res) => {
-  res.json(dogs[parseInt(req.params.id)-1]);
+  res.json(dogs[parseInt(req.params.id) - 1]);
 });
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
-})
+  console.log(`Listening on port ${port}`);
+});
