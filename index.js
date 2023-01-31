d828fd32ede9789c4a388acf062ec95a10d73c72
Noah Böhringer <noahboehringer@Noahs-MacBook-Air.local>
2023-01-31 14:16:31 +0200

    second commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express'
+
+const app = express()
+
+const port = 4567
+
+const dogs = [
+    {name: "Sam", breed: "Husky"}, 
+    {name: "Josh", breed: "Lab"}
+]
+
+app.get("/", (req, res) => {
+    res.json(dogs)
+})
+
+app.get("/dogs/:id", (req, res) => {
+  res.json(dogs[parseInt(req.params.id)-1]);
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
