67f2f78f07c6814f541595eb4b4b12b6e61fa043
Noah Böhringer <noahboehringer@Noahs-MacBook-Air.local>
2023-02-02 13:39:34 +0200

    changed to UserBackend

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
 import express from "express";
-
+import bodyParser from "body-parser";
 const app = express();
 
-const port = 4567;
+const users = [];
 
-const dogs = [
-  {name: "Sam", breed: "Husky"},
-  {name: "Josh", breed: "Lab"},
-  {name: "Basti", breed: "Retriever"},
-];
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.get("/", (req, res) => {
-  res.json(dogs);
+// parse application/json
+app.use(bodyParser.json());
+
+app.get("/api/data", (req, res) => {
+  res.json({users});
 });
 
-app.get("/dogs/:id", (req, res) => {
-  res.json(dogs[parseInt(req.params.id) - 1]);
+app.post("/api/data", (req, res) => {
+  const {email, password} = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({message: "Email and password required"});
+  }
+
+  users.push({email, password});
+  res.json({message: `User with email ${email} added`});
 });
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+app.put("/api/data/:index", (req, res) => {
+  const {index} = req.params;
+  const {email, password} = req.body;
+
+  if (index >= users.length) {
+    return res.status(400).json({message: "Index out of range"});
+  }
+
+  users[index] = {email, password};
+  res.json({message: `User at index ${index} updated with email ${email}`});
 });
+
+const port = process.env.PORT || 4567;
+app.listen(port, () => console.log(`API running on port ${port}`));
